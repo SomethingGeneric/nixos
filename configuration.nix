718514adf55d707026f2a0d819810c3b47ecb63f
matt c <matt@xhec.dev>
2024-04-03 12:40:09 -0400

    stuffs

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
     ./hardware-configuration.nix
     ./hacker.nix
     ./games.nix
+    ./gnome.nix
   ];
 
   # -----------
@@ -48,24 +49,6 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome = {
-  	enable = true;
-  	# nightmare for fractional scaling
-  	extraGSettingsOverrides = ''
-  	  [org.gnome.mutter]
-  	  experimental-features=['scale-monitor-framebuffer']
-  	'';
-  	extraGSettingsOverridePackages = [ pkgs.gnome.mutter ];
-  };
-  nixpkgs.config.firefox.enableGnomeExtensions = true;
-  services.gnome.gnome-browser-connector.enable = true;
-
-  # TODO: this is to mitigate a dep warning by the firefox extensions line above
-  # but clearly i misunderstand the syntax because it doesn't work lol
-  # nativeMessagingHosts.packages = [ pkgs.gnome-browser-connector ];
 
   # Configure keymap in X11
   services.xserver = {
@@ -131,12 +114,6 @@
      obsidian
      pkgs.vscode-fhs
      slack
-
-     # gnome suffering
-     pkgs.gnome.gnome-tweaks # why tf is this in pkgs.gnome but below isn't ??
-     pkgs.gnomeExtensions.dash-to-dock
-     pkgs.gnomeExtensions.caffeine
-     pkgs.gnomeExtensions.gsconnect
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
